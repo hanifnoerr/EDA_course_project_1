1871e0e6805c9be553b448ab464dacdf731d06fb
Hanif Noer Rofiq <69993542+hanifnoerr@users.noreply.github.com>
2022-02-04 18:06:29 +0700

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -24,5 +24,5 @@ t$dateTime <- as.POSIXct(dateTime)
 plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
  
 # for saving the plot with specifict size
-dev.copy(png, "plot1.png", width=480, height=480)
+dev.copy(png, "plot2.png", width=480, height=480)
 dev.off()
